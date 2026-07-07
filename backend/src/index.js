fd968b89db6cd234f1801fbd18b0e0aad4aa2819
HsrRaza <hr.raza04@gmail.com>
2026-07-07 14:07:41 +0530

    learn socket io on emit and implemented state user based action

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,53 @@
 import express from "express"
+import dotenv from "dotenv"
+import http from "http"
+import cors from "cors"
+
+import { Server } from "socket.io"
 
 
-const app =express()
 
-app.listen(40001 , ()=>{
-    console.log(`server is running`);
-    
+
+
+
+dotenv.config({})
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+const port = process.env.PORT
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+
+io.on("connection", (socket) => {
+    console.log("New user connected ", socket.id);
+
+
+    socket.on("send-message", (message) => {
+
+
+        console.log(message);
+
+        io.emit("receive-message", message)
+
+
+
+
+    })
+
+
+})
+
+
+
+server.listen(port, () => {
+    console.log(`server is running on ${port}`);
+
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,121 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect } from "react"
+import { socket } from "./socket"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const [message, setMessage] = useState([])
+
+  const [user, setUser] = useState("")
+  const [text, setText] = useState("")
+  const [join ,setJoin] = useState(false)
+
+
+  const joinRoom = ()=>{
+    if(!user.trim()) return
+   
+    setJoin(true)
+
+
+
+  }
+
+
+
+
+
+  const sendBtn = () => {
+
+    if (!text.trim()) return;
+
+    socket.emit("send-message", {
+      user: user,
+      text: text
+    })
+
+
+    setText("")
+  }
+
+
+
+
+  useEffect(() => {
+
+    socket.on("receive-message", (message) => {
+      console.log(message);
+
+      setMessage((prev) => [...prev, message])
+
+    })
+
+    return () => {
+      socket.off("receive-message")
+    }
+
+  }, [])
+
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+    <div>Chat app
+
+     {
+      !join && 
+       <div>
+
+        <label htmlFor="">Username</label>
+        <input type="text" value={user}
+        onChange={(e)=> setUser(e.target.value)}
+         />
+
+         <button onClick={joinRoom}>join </button>
+      </div>
+     }
+
+
+{
+  join && <div>
+
+      <div>
+
+
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          <input type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={sendBtn}>send</button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        </div>
+
+        <div>
+          {
+            message.map((msg, index) => (
+              <div key={index}>
+                <h3>{msg.user}</h3>
+                <p>{msg.text}</p>
+              </div>
+            ))
+          }
+        </div>
+
+      </div>
+
+  </div>
+}
+
+
+    
+
+    </div>
   )
 }
 
